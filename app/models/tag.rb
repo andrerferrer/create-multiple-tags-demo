6c0327e77b0edc1dd93e66ae9521fc020ed15f8e
Andre Ferrer <andrerferrer@hotmail.com>
2020-06-23 17:14:21 -0300

    Remove no longer used tag params

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,4 @@
 class Tag < ApplicationRecord
-  STRONG_PARAMS = %i[
-    name
-  ]
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   has_many :restaurant_tags, dependent: :destroy
   has_many :restaurants, through: :restaurant_tags
