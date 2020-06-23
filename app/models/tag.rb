444e54738d22ecc1bef408372c5ce08efa36ac1e
Andre Ferrer <andrerferrer@hotmail.com>
2020-06-23 16:54:43 -0300

    Add dependency to Tag model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,6 +3,6 @@ class Tag < ApplicationRecord
     name
   ]
   validates :name, presence: true, uniqueness: { case_sensitive: false }
-  has_many :restaurant_tags
+  has_many :restaurant_tags, dependent: :destroy
   has_many :restaurants, through: :restaurant_tags
 end
