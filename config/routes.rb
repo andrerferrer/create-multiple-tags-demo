da5e3db0d6aac9ee0ff5fe598f59f29fbc7c1031
Andre Ferrer <andrerferrer@hotmail.com>
2020-06-23 16:52:48 -0300

    add tags#destroy routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
     resources :reviews, only: :create
   end
 
-  resources :tags, only: %i[new create]
+  resources :tags, only: %i[new create destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
