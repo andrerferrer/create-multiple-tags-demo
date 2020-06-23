7c25083c71c079cf68c3998fd7c342bc7a505b04
Andre Ferrer <andrerferrer@hotmail.com>
2020-06-23 16:35:10 -0300

    Implement tags controller new and create

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,16 @@
 class TagsController < ApplicationController
   def new
+    @tag = Tag.new
   end
 
   def create
+    @tag = Tag.new strong_params
+    @tag.save ? redirect_to(root_path, notice: "Tag #{@tag.name} was succesfully created") : render(:new)
+  end
+
+  private
+
+  def strong_params
+    params.require(:tag).permit(Tag::STRONG_PARAMS)
   end
 end
