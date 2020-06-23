106766cf3b96d47e93c88c754ff218960abeecef
Andre Ferrer <andrerferrer@hotmail.com>
2020-06-23 16:52:26 -0300

    Implement tags controller destroy

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,11 +1,21 @@
 class TagsController < ApplicationController
   def new
     @tag = Tag.new
+    @tags = Tag.all
   end
 
   def create
     @tag = Tag.new strong_params
+    @tags = Tag.all
     @tag.save ? redirect_to(root_path, notice: "Tag #{@tag.name} was succesfully created") : render(:new)
+  end
+
+  def destroy
+    @tag = Tag.find params[:id]
+    @tag.destroy
+    respond_to do |format|
+      format.js {render inline: "location.reload();" }
+    end
   end
 
   private
